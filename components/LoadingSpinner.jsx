90bf1c160d9ade17c3815c1c7c55827b3d55af14
pk102651 <pk102651@gmail.com>
2026-09-16 12:13:55 +0530

    Add LoadingSpinner component

---
+++ components/LoadingSpinner.jsx
@@ -0,0 +1,8 @@
+// components/LoadingSpinner.jsx
+export default function LoadingSpinner() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-secondary"></div>
+    </div>
+  )
+}
